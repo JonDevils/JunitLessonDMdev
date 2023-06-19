c76d66cb492d60d8fdf64ee5c6f778de230a3063
JonDevils <jondevils@yandex.ru>
2023-06-19 20:23:04 +0300

    170623 933

--- src/test/java/com/example/junitlessondmdev/user/UserServiceTest.java
+++ src/test/java/com/example/junitlessondmdev/user/UserServiceTest.java
@@ -2,44 +2,58 @@ package com.example.junitlessondmdev.user;
 
 import com.example.junitlessondmdev.dto.User;
 
+import com.example.junitlessondmdev.paramersolver.UserServiceParamResolver;
 import lombok.NoArgsConstructor;
 import net.bytebuddy.implementation.bind.annotation.IgnoreForBinding;
+import org.assertj.core.api.MatcherAssert;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.*;
 
+import java.lang.reflect.Parameter;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Stream;
 
+import static java.util.Optional.empty;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 @Tag("fast")
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)//порядок запуска методов
 //methodname - по порядку алфавит. displayname - и аннотация @Displayname("") - меняет название
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)//Per_class - создаем один класс а не множество как с Per_metod (т.е статик можно не делать)
+@ExtendWith({
+        UserServiceParamResolver.class
+})
 public class UserServiceTest { //public только в этом случае
 
     private static UserService userService;
 
 
-    private static final User IVAN =  User.of(1L,"ivan","123");
-    private static final User PETR =  User.of(2L,"petr","111");
+    private static final User IVAN =  User.of(1L,"Ivan","123");
+    private static final User PETR =  User.of(2L,"Petr","111");
+    UserServiceTest(TestInfo testInfo) {
+        System.out.println( );
+    }
     @BeforeAll
     void init () {
         System.out.println("BeforeAll: "+ this );
     } // не статик т.к per_class
 
     @BeforeEach
-    void prepare() {
+    void prepare(UserService userService) {
         System.out.println("Before each: " + this);
-//        userService - new UserService();
+        this.userService = userService;
     }
 
     @Test
     @Order(1)//вместе с MethodOrderer.OrderAnnotation
-    public void userEmptyIfNoUserAdded() {
+    public void userEmptyIfNoUserAdded(UserService userService) {
         System.out.println("test 1 : " + this);
-    var userServicef = new UserService();
-    var users = userServicef.getAll();
+    var users =  userService.getAll();
+        System.out.println(userService.getAll());
     assertTrue(users.isEmpty(),()-> "Не нулевое значение");
     }
 
@@ -58,10 +72,11 @@ public class UserServiceTest { //public только в этом случае
     assertThat(users).hasSize(2);
 //    assertEquals(2,users.size());
     }
-
-    @Test
-
-    void LoginSuccessIfUserExists() {
+@Test
+void TestFunc
+//    @Test
+@RepeatedTest(value = 2,name = RepeatedTest.LONG_DISPLAY_NAME)
+    void LoginSuccessIfUserExists(RepetitionInfo repetitionInfo) {
         UserService us = new UserService();
         us.add(IVAN);
         Optional<User> mbuser =  us.login(IVAN.getUsername(),IVAN.getPassword());
@@ -143,6 +158,27 @@ public class UserServiceTest { //public только в этом случае
         }
 
 
+    }
+    @ParameterizedTest()
+//    @ArgumentsSource()
+//    @NullSource//поставляет null в наш параметр ! но только для одного парметра
+//    @EmptySource
+//    @NullAndEmptySource
+//    @ValueSource (strings = {"Ivan","Petr"})
+    @MethodSource("getArgumentsForLoginTest")
+    void loginParametrizedTest(String username,String password,Optional<User>user) {
+        userService.add(IVAN,PETR);
+
+      var myb =  userService.login(username,password);
+      assertThat(myb).isEqualTo(user);
+    }
+    static Stream<Arguments> getArgumentsForLoginTest() {
+        return Stream.of(
+               Arguments.of( "Ivan","123",Optional.of(IVAN)),
+                Arguments.of( "Petr","111",Optional.of(PETR)),
+                Arguments.of("Petr","der",Optional.empty()),
+                Arguments.of( "der","111",Optional.empty())
+        );
     }
 
 }
